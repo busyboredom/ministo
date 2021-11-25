6125031c799712d8db6ee2aed9355f45bc306b73
Charlie Wilkin <charlie@busyboredom.com>
2021-11-25 14:32:10 -0500

    Add basic rust command

--- src-tauri/src/main.rs
+++ src-tauri/src/main.rs
@@ -3,8 +3,21 @@
   windows_subsystem = "windows"
 )]
 
+use env_logger;
+use log::info;
+use tauri::command;
+
+#[command(async)]
+fn print_status() -> String {
+  info!("Running");
+  "Running".to_string()
+}
+
 fn main() {
+  env_logger::init();
+
   tauri::Builder::default()
+    .invoke_handler(tauri::generate_handler![print_status])
     .run(tauri::generate_context!())
     .expect("error while running tauri application");
 }
